4071a54ca6cbd4be16842f24082109633e6405ba
santiagogustavo <santiago.gustavo.mail@gmail.com>
2023-01-23 21:23:44 -0300

    feat(dotenv): add env file, cleanup api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 app.use(
@@ -10,45 +11,16 @@ app.use(
 
 const { postSearch } = require('./services/ifood');
 
-const ingredients = [
-  {
-    id: '1',
-    item: 'Bacon',
-  },
-  {
-    id: '2',
-    item: 'Eggs',
-  },
-  {
-    id: '3',
-    item: 'Milk',
-  },
-  {
-    id: '4',
-    item: 'Butter',
-  },
-];
-
-app.get('/ingredients', (req, res) => {
-  res.send(ingredients);
-});
-
-app.get('/search', (req, res) => {
-  postSearch({
-    alias: 'CMS_RESULTS_ONE_PAGE',
-    latitude: -23.194406196479143,
-    longitude: -46.907479115793215,
-    term: 'pizza',
-    size: 20,
-  })
+app.post('/search', (req, res) => {
+  postSearch(req.query)
     .then(({ data }) => {
-      console.log(data);
+      res.status(201);
       res.send(data);
     })
     .catch(error => {
-      console.log(error);
+      res.status(500);
       res.send(error);
     });
 });
 
-app.listen(6069);
+app.listen(process.env.SERVER_PORT);
