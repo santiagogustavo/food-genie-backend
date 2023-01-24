9497d3ae17b0ca920e2ba6a9de91037c1f74dffb
santiagogustavo <santiago.gustavo.mail@gmail.com>
2023-01-23 21:16:24 -0300

    feat(linter): add eslint + prettier + configs

--- constants/urls.js
+++ constants/urls.js
@@ -1,6 +1,6 @@
 // page: number
 // size: number
-const ORDERS = "/v4/customers/me/orders?page={page}&size={size}";
+const ORDERS = '/v4/customers/me/orders?page={page}&size={size}';
 
 // alias: CMS_RESULTS_MERCHANTS, CMS_RESULTS_ONE_PAGE
 // latitude: number
@@ -9,7 +9,7 @@ const ORDERS = "/v4/customers/me/orders?page={page}&size={size}";
 // term: string
 // size: number
 // categories: string (categories from filter)
-const SEARCH = "/v2/cardstack/search/results";
+const SEARCH = '/v2/cardstack/search/results';
 
 module.exports = {
   ORDERS,
--- index.js
+++ index.js
@@ -1,52 +1,54 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 app.use(
   cors({
-    origin: "*",
+    origin: '*',
   })
 );
 
-const { postSearch } = require("./services/ifood");
+const { postSearch } = require('./services/ifood');
 
 const ingredients = [
   {
-    id: "1",
-    item: "Bacon",
+    id: '1',
+    item: 'Bacon',
   },
   {
-    id: "2",
-    item: "Eggs",
+    id: '2',
+    item: 'Eggs',
   },
   {
-    id: "3",
-    item: "Milk",
+    id: '3',
+    item: 'Milk',
   },
   {
-    id: "4",
-    item: "Butter",
+    id: '4',
+    item: 'Butter',
   },
 ];
 
-app.get("/ingredients", (req, res) => {
+app.get('/ingredients', (req, res) => {
   res.send(ingredients);
 });
 
-app.get("/search", (req, res) => {
+app.get('/search', (req, res) => {
   postSearch({
-    alias: "CMS_RESULTS_ONE_PAGE",
+    alias: 'CMS_RESULTS_ONE_PAGE',
     latitude: -23.194406196479143,
     longitude: -46.907479115793215,
-    term: "pizza",
+    term: 'pizza',
     size: 20,
-  }).then(({ data }) => {
-    console.log(data);
-    res.send(data);
-  }).catch(error => {
-    console.log(error);
-    res.send(error);
-  });
+  })
+    .then(({ data }) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch(error => {
+      console.log(error);
+      res.send(error);
+    });
 });
 
 app.listen(6069);
